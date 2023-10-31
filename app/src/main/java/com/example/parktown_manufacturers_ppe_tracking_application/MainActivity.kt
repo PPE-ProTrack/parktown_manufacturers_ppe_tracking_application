29f87f952e77309ad96d1609212d83a615aedb0b
St10092007 <129759981+St10092007@users.noreply.github.com>
2023-11-01 01:12:11 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/parktown_manufacturers_ppe_tracking_application/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parktown_manufacturers_ppe_tracking_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
