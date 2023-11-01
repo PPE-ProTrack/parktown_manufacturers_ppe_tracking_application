689c97fcf5d9dc61a4a42529d4e68fb51a5ee3b6
St10092007 <129759981+St10092007@users.noreply.github.com>
2023-11-01 02:22:17 +0200

    Workflow test

--- app/src/main/java/com/example/parktown_manufacturers_ppe_tracking_application/MainActivity.kt
+++ app/src/main/java/com/example/parktown_manufacturers_ppe_tracking_application/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.parktown_manufacturers_ppe_tracking_application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// main activity
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
